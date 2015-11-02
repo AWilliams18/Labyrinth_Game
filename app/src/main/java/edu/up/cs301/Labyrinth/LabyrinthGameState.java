5011c17e9efb51d2d8289b99839da699e4aa44f0
CelestiaTheDryad <bmtmail@comcast.net>
2015-11-01 16:45:18 -0800

    Labyrinth package and blank gamestate

---
+++ app/src/main/java/edu/up/cs301/Labyrinth/LabyrinthGameState.java
@@ -0,0 +1,9 @@
+package edu.up.cs301.Labyrinth;
+
+import edu.up.cs301.game.infoMsg.GameState;
+
+/**
+ * Created by Brendan on 11/1/2015.
+ */
+public class LabyrinthGameState extends GameState {
+}
