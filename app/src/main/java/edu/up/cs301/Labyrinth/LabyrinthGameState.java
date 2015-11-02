e72e62d3c2ed3ea348b9fa58ab908a669fc1d187
CelestiaTheDryad <bmtmail@comcast.net>
2015-11-01 17:39:19 -0800

    LabyrinthGameState default and copy constructors.

--- app/src/main/java/edu/up/cs301/Labyrinth/LabyrinthGameState.java
+++ app/src/main/java/edu/up/cs301/Labyrinth/LabyrinthGameState.java
@@ -25,9 +25,36 @@ public class LabyrinthGameState extends GameState {
         player4Targets = new String[4];
     }
 
+    //creates a deep copy of a given game state
     public LabyrinthGameState (LabyrinthGameState toCopy) {
         currentPlayer = toCopy.currentPlayer;
         extraTile = new Tile(toCopy.extraTile);
-        //for(int i = 0; i < 7; i++)
+
+        gameTiles = new Tile[7][7];
+        for(int i = 0; i < 7; i++) {
+            for(int j = 0; j < 7; j++) {
+                gameTiles[i][j] = new Tile(toCopy.gameTiles[i][j]);
+            }
+        }
+
+        player1Targets = new String[toCopy.player1Targets.length];
+        for(int i = 0; i < player1Targets.length; i++) {
+            player1Targets[i] = toCopy.player1Targets[i];
+        }
+
+        player2Targets = new String[toCopy.player2Targets.length];
+        for(int i = 0; i < player2Targets.length; i++) {
+            player2Targets[i] = toCopy.player2Targets[i];
+        }
+
+        player3Targets = new String[toCopy.player3Targets.length];
+        for(int i = 0; i < player3Targets.length; i++) {
+            player3Targets[i] = toCopy.player3Targets[i];
+        }
+
+        player4Targets = new String[toCopy.player4Targets.length];
+        for(int i = 0; i < player4Targets.length; i++) {
+            player4Targets[i] = toCopy.player4Targets[i];
+        }
     }
 }
