e3243666d9115a0e3eec2d2e283ef94738ae873d
CelestiaTheDryad <bmtmail@comcast.net>
2015-11-01 17:07:26 -0800

    Gamestate and Tile instance variables.

--- app/src/main/java/edu/up/cs301/Labyrinth/LabyrinthGameState.java
+++ app/src/main/java/edu/up/cs301/Labyrinth/LabyrinthGameState.java
@@ -6,4 +6,11 @@ import edu.up.cs301.game.infoMsg.GameState;
  * Created by Brendan on 11/1/2015.
  */
 public class LabyrinthGameState extends GameState {
+    private int currentPlayer;
+    private Tile[][] gameTiles;
+    private Tile extraTile;
+    private String[] player1Targets;
+    private String[] player2Targets;
+    private String[] player3Targets;
+    private String[] player4Targets;
 }
